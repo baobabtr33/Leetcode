f47694a64f420aafef0d9ff81bcaf7af95fd132a
JungHwan Kim <stevekim01310@gmail.com>
2023-04-05 01:36:39 +0900

    Create 6_Zigzag_Conversion.py

---
+++ src/6_Zigzag_Conversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, num_rows: int) -> str:
+        # i, r -> 0 * (r - 1) + i, 2 * (r - 1) - i, 2 * (r - 1) + i, 4 * (r - 1) - i, 4 * (r - 1) + i, ....
+        def i_row_indices(i: int, r: int) -> Iterator[int]:
+            if r == 1: yield from count(i)
+            
+            yield i            
+            for x in count(2 * (r - 1), 2 * (r - 1)):
+                yield from (x - i, x + i) if 0 < i < r - 1 else (x + i,)
+        
+        return ''.join(chain.from_iterable(
+            (s[index] for index in takewhile(lambda x: x < len(s), i_row_indices(i, num_rows)))
+            for i in range(num_rows)
+        ))
