08908ad093aede26f372e7ce1fbec162bc42dfa6
JungHwan Kim <stevekim01310@gmail.com>
2023-04-05 01:37:31 +0900

    Create 7_Reverse_Integer.py

---
+++ src/7_Reverse_Integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse(self, x):
+        if x != 0:
+            sign = int(x / abs(x))
+            m = int(str(abs(x))[::-1])
+        else:
+            sign = 0
+            m = 0
+            
+        if m <= 2**31:
+            return sign * m
+        else: 
+            return 0
